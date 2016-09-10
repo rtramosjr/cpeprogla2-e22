8c927a03c282ce501643c0a9c0716c003a5374f3
Ronaldo T. Ramos Jr <rtramosjr@gmail.com>
2016-09-10 11:41:17 +0800

    Add StrCat

--- lab3.cpp
+++ lab3.cpp
@@ -3,28 +3,35 @@
 using namespace std;
 
 char* myStrCmp(char* str1, char* str2){
+  
   switch(strcmp(str1, str2)){
-  	case -1:
+  	
+	case -1:
   		cout<<"negative";
   		break;
-  	case 0:
+  
+   	case 0:
   		cout<<"equal";
   		break;
+  
   	case 1:
   		cout<<"positive";
   		break;
   }
 }
-void myStrCpy (char* str1, char*str2){
+	void myStrCpy (char* str1, char*str2){
 	strcpy(str1, str2);
 	
 }
-
+	void myStrCat (char* str1, char*str2){
+	strcpy(str1, str2);
+}
 int main(){
   char str1[100]="Hello";
   char str2[100]="hello";
   
   myStrCmp(str1, str2);
   myStrCpy(str1, str2);
+  myStrCat(str1, str2);
   cout<<str1;
 }
